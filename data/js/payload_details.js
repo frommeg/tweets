4550550ae6e961e7cab1f30a3332eeb11287d6b4
Fromm <fromm.fancafe@gmail.com>
2020-03-09 00:17:07 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5001,
-	"created_at": "Sat Mar 07 15:17:05 +0000 2020",
+	"tweets": 5002,
+	"created_at": "Sun Mar 08 15:17:06 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,286 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Sun Mar 08 12:01:21 +0000 2020",
+		"id": 1236623036524175400,
+		"id_str": "1236623036524175360",
+		"full_text": "【 #Melon_Ticket 】\n［프롬의 봄 뮤직＆토크 스테이지］ at KB아트홀 강남 (2020.04.26 - 2020.04.26)\nhttps://t.co/TikbJFS12c\n　\n#프롬 #Fromm #이유진\n#멜론티켓 #공연 #공연안내 https://t.co/M7s1Gyj4SI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			132
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Melon_Ticket",
+					"indices": [
+						2,
+						15
+					]
+				},
+				{
+					"text": "프롬",
+					"indices": [
+						101,
+						104
+					]
+				},
+				{
+					"text": "Fromm",
+					"indices": [
+						105,
+						111
+					]
+				},
+				{
+					"text": "이유진",
+					"indices": [
+						112,
+						116
+					]
+				},
+				{
+					"text": "멜론티켓",
+					"indices": [
+						117,
+						122
+					]
+				},
+				{
+					"text": "공연",
+					"indices": [
+						123,
+						126
+					]
+				},
+				{
+					"text": "공연안내",
+					"indices": [
+						127,
+						132
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/TikbJFS12c",
+					"expanded_url": "https://ticket.melon.com/performance/index.htm?prodId=205102",
+					"display_url": "ticket.melon.com/performance/in…",
+					"indices": [
+						75,
+						98
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1236623035354026000,
+					"id_str": "1236623035354025984",
+					"indices": [
+						133,
+						156
+					],
+					"media_url": "http://pbs.twimg.com/media/ESleGUdXkAApinz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ESleGUdXkAApinz.jpg",
+					"url": "https://t.co/M7s1Gyj4SI",
+					"display_url": "pic.twitter.com/M7s1Gyj4SI",
+					"expanded_url": "https://twitter.com/Fromm_Fancafe/status/1236623036524175360/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 420,
+							"h": 594,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 420,
+							"h": 594,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 420,
+							"h": 594,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1236623035354026000,
+					"id_str": "1236623035354025984",
+					"indices": [
+						133,
+						156
+					],
+					"media_url": "http://pbs.twimg.com/media/ESleGUdXkAApinz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ESleGUdXkAApinz.jpg",
+					"url": "https://t.co/M7s1Gyj4SI",
+					"display_url": "pic.twitter.com/M7s1Gyj4SI",
+					"expanded_url": "https://twitter.com/Fromm_Fancafe/status/1236623036524175360/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 420,
+							"h": 594,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 420,
+							"h": 594,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 420,
+							"h": 594,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2202738804,
+			"id_str": "2202738804",
+			"name": "프롬(Fromm) 공식팬카페",
+			"screen_name": "Fromm_Fancafe",
+			"location": "",
+			"description": "🎤 싱어송라이터 #프롬 #Fromm 팬카페\nhttps://t.co/PpLCrjIH7p\n\n[공식]\n트위터 @Frommeg\n페이스북 https://t.co/0zQF0hUdie\n인스타그램 https://t.co/gzqlcz3Cdx\n유튜브 https://t.co/SoRgUCTjtF\n직캠 https://t.co/fF7J3ggg5Y",
+			"url": "https://t.co/mUr4gVk6So",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/mUr4gVk6So",
+							"expanded_url": "http://cafe.daum.net/frommeg",
+							"display_url": "cafe.daum.net/frommeg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/PpLCrjIH7p",
+							"expanded_url": "http://frommeg.com",
+							"display_url": "frommeg.com",
+							"indices": [
+								24,
+								47
+							]
+						},
+						{
+							"url": "https://t.co/0zQF0hUdie",
+							"expanded_url": "http://facebook.com/fromm12",
+							"display_url": "facebook.com/fromm12",
+							"indices": [
+								72,
+								95
+							]
+						},
+						{
+							"url": "https://t.co/gzqlcz3Cdx",
+							"expanded_url": "http://instagram.com/frommeg",
+							"display_url": "instagram.com/frommeg",
+							"indices": [
+								102,
+								125
+							]
+						},
+						{
+							"url": "https://t.co/SoRgUCTjtF",
+							"expanded_url": "http://youtube.com/user/pparful",
+							"display_url": "youtube.com/user/pparful",
+							"indices": [
+								130,
+								153
+							]
+						},
+						{
+							"url": "https://t.co/fF7J3ggg5Y",
+							"expanded_url": "http://fancam.frommeg.com",
+							"display_url": "fancam.frommeg.com",
+							"indices": [
+								157,
+								180
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 304,
+			"friends_count": 27,
+			"listed_count": 4,
+			"created_at": "Tue Nov 19 08:38:40 +0000 2013",
+			"favourites_count": 2862,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4240,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1192311683152109568/MfVUtlJj_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1192311683152109568/MfVUtlJj_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2202738804/1573104234",
+			"profile_link_color": "865C74",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "【 #Melon_Ticket 】\n［프롬의 봄 뮤직＆토크 스테이지］ at KB아트홀 강남 (2020.04.26 - 2020.04.26)\nhttps://t.co/TikbJFS12c\n　\n#프롬 #Fromm #이유진\n#멜론티켓 #공연 #공연안내 https://t.co/M7s1Gyj4SI"
+	},
+	{
 		"created_at": "Sat Mar 07 04:38:09 +0000 2020",
 		"id": 1236149111327465500,
 		"id_str": "1236149111327465473",
