786b3f30ac35eea4d26f4b58d29dee43e70fe434
Fromm <fromm.fancafe@gmail.com>
2020-08-23 00:17:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5105,
-	"created_at": "Mon Aug 17 15:17:05 +0000 2020",
+	"tweets": 5106,
+	"created_at": "Sat Aug 22 15:17:05 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_08.js
+++ data/js/tweets/2020_08.js
@@ -1,6 +1,240 @@
 Grailbird.data.tweets_2020_08 = 
 [
 	{
+		"created_at": "Sat Aug 22 12:24:25 +0000 2020",
+		"id": 1297147613242896400,
+		"id_str": "1297147613242896384",
+		"full_text": "【 #Melon 】\nCOMINNG SOON New Album!\nhttps://t.co/MEDOoGalNG\n\n#프롬 #Fromm #이유진\n#멜론 #포토 #멜론포토 https://t.co/YZ0MzOAUwQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			89
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Melon",
+					"indices": [
+						2,
+						8
+					]
+				},
+				{
+					"text": "프롬",
+					"indices": [
+						60,
+						63
+					]
+				},
+				{
+					"text": "Fromm",
+					"indices": [
+						64,
+						70
+					]
+				},
+				{
+					"text": "이유진",
+					"indices": [
+						71,
+						75
+					]
+				},
+				{
+					"text": "멜론",
+					"indices": [
+						76,
+						79
+					]
+				},
+				{
+					"text": "포토",
+					"indices": [
+						80,
+						83
+					]
+				},
+				{
+					"text": "멜론포토",
+					"indices": [
+						84,
+						89
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/MEDOoGalNG",
+					"expanded_url": "https://www.melon.com/artist/photo_detail.htm?artistId=570302&photoId=80108824&orderBy=NEW",
+					"display_url": "melon.com/artist/photo_d…",
+					"indices": [
+						35,
+						58
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1297147611217092600,
+					"id_str": "1297147611217092608",
+					"indices": [
+						90,
+						113
+					],
+					"media_url": "http://pbs.twimg.com/media/EgBk4TvXYAAe9x9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EgBk4TvXYAAe9x9.jpg",
+					"url": "https://t.co/YZ0MzOAUwQ",
+					"display_url": "pic.twitter.com/YZ0MzOAUwQ",
+					"expanded_url": "https://twitter.com/Fromm_Fancafe/status/1297147613242896384/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1297147611217092600,
+					"id_str": "1297147611217092608",
+					"indices": [
+						90,
+						113
+					],
+					"media_url": "http://pbs.twimg.com/media/EgBk4TvXYAAe9x9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EgBk4TvXYAAe9x9.jpg",
+					"url": "https://t.co/YZ0MzOAUwQ",
+					"display_url": "pic.twitter.com/YZ0MzOAUwQ",
+					"expanded_url": "https://twitter.com/Fromm_Fancafe/status/1297147613242896384/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 1000,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2202738804,
+			"id_str": "2202738804",
+			"name": "프롬(Fromm) 공식팬카페",
+			"screen_name": "Fromm_Fancafe",
+			"location": "",
+			"description": "🎤 싱어송라이터 #프롬 #Fromm 팬카페",
+			"url": "https://t.co/mUr4gVk6So",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/mUr4gVk6So",
+							"expanded_url": "http://cafe.daum.net/frommeg",
+							"display_url": "cafe.daum.net/frommeg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 288,
+			"friends_count": 26,
+			"listed_count": 4,
+			"created_at": "Tue Nov 19 08:38:40 +0000 2013",
+			"favourites_count": 2806,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4358,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1192311683152109568/MfVUtlJj_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1192311683152109568/MfVUtlJj_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2202738804/1573104234",
+			"profile_link_color": "865C74",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 9,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "【 #Melon 】\nCOMINNG SOON New Album!\nhttps://t.co/MEDOoGalNG\n\n#프롬 #Fromm #이유진\n#멜론 #포토 #멜론포토 https://t.co/YZ0MzOAUwQ"
+	},
+	{
 		"created_at": "Mon Aug 17 06:49:15 +0000 2020",
 		"id": 1295251327505830000,
 		"id_str": "1295251327505829889",
