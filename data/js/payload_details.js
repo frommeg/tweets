aa7a86dda3bb2c7bc5d81b433e5d404897312624
Fromm <fromm.fancafe@gmail.com>
2020-10-23 00:17:07 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5151,
-	"created_at": "Sat Oct 17 15:17:05 +0000 2020",
+	"tweets": 5152,
+	"created_at": "Thu Oct 22 15:17:06 +0000 2020",
 	"lang": "ko"
 }
--- data/js/tweets/2020_10.js
+++ data/js/tweets/2020_10.js
@@ -1,6 +1,240 @@
 Grailbird.data.tweets_2020_10 = 
 [
 	{
+		"created_at": "Thu Oct 22 06:16:31 +0000 2020",
+		"id": 1319160687298547700,
+		"id_str": "1319160687298547712",
+		"full_text": "【 #Bugs 】\n[뮤직포커스] 국내 최초! 앨범 아트가 살아 움직입니다! (2020.08.03)\nhttps://t.co/r5dcxFY0rB\n\n#프롬 #Fromm #이유진\n#벅스 #스페셜 #벅스스페셜 https://t.co/yKpt1VxiGp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			111
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Bugs",
+					"indices": [
+						2,
+						7
+					]
+				},
+				{
+					"text": "프롬",
+					"indices": [
+						80,
+						83
+					]
+				},
+				{
+					"text": "Fromm",
+					"indices": [
+						84,
+						90
+					]
+				},
+				{
+					"text": "이유진",
+					"indices": [
+						91,
+						95
+					]
+				},
+				{
+					"text": "벅스",
+					"indices": [
+						96,
+						99
+					]
+				},
+				{
+					"text": "스페셜",
+					"indices": [
+						100,
+						104
+					]
+				},
+				{
+					"text": "벅스스페셜",
+					"indices": [
+						105,
+						111
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/r5dcxFY0rB",
+					"expanded_url": "https://music.bugs.co.kr/musicpost/QHEQ6K2BAC7TN1C3HSZW?&wl_ref=list_sp_03_sp",
+					"display_url": "music.bugs.co.kr/musicpost/QHEQ…",
+					"indices": [
+						55,
+						78
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1319160686136754200,
+					"id_str": "1319160686136754176",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/Ek6Zp6LXgAATbXu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ek6Zp6LXgAATbXu.jpg",
+					"url": "https://t.co/yKpt1VxiGp",
+					"display_url": "pic.twitter.com/yKpt1VxiGp",
+					"expanded_url": "https://twitter.com/Fromm_Fancafe/status/1319160687298547712/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 722,
+							"h": 260,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 245,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 722,
+							"h": 260,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1319160686136754200,
+					"id_str": "1319160686136754176",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/Ek6Zp6LXgAATbXu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ek6Zp6LXgAATbXu.jpg",
+					"url": "https://t.co/yKpt1VxiGp",
+					"display_url": "pic.twitter.com/yKpt1VxiGp",
+					"expanded_url": "https://twitter.com/Fromm_Fancafe/status/1319160687298547712/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 722,
+							"h": 260,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 245,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 722,
+							"h": 260,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2202738804,
+			"id_str": "2202738804",
+			"name": "프롬(Fromm) 공식팬카페",
+			"screen_name": "Fromm_Fancafe",
+			"location": "",
+			"description": "🎤 싱어송라이터 #프롬 #Fromm 팬카페",
+			"url": "https://t.co/mUr4gVk6So",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/mUr4gVk6So",
+							"expanded_url": "http://cafe.daum.net/frommeg",
+							"display_url": "cafe.daum.net/frommeg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 296,
+			"friends_count": 26,
+			"listed_count": 5,
+			"created_at": "Tue Nov 19 08:38:40 +0000 2013",
+			"favourites_count": 2763,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4413,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1192311683152109568/MfVUtlJj_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1192311683152109568/MfVUtlJj_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2202738804/1573104234",
+			"profile_link_color": "865C74",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "【 #Bugs 】\n[뮤직포커스] 국내 최초! 앨범 아트가 살아 움직입니다! (2020.08.03)\nhttps://t.co/r5dcxFY0rB\n\n#프롬 #Fromm #이유진\n#벅스 #스페셜 #벅스스페셜 https://t.co/yKpt1VxiGp"
+	},
+	{
 		"created_at": "Sat Oct 17 06:46:32 +0000 2020",
 		"id": 1317356303451361300,
 		"id_str": "1317356303451361281",
