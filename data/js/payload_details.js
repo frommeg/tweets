62a4d8799c35125211f5bff3f1320484d21291d9
Fromm <fromm.fancafe@gmail.com>
2020-12-15 00:17:07 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5159,
-	"created_at": "Thu Nov 05 15:17:06 +0000 2020",
+	"tweets": 5160,
+	"created_at": "Mon Dec 14 15:17:06 +0000 2020",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2020_12.js
@@ -0,0 +1,197 @@
+Grailbird.data.tweets_2020_12 = 
+[
+	{
+		"created_at": "Mon Dec 14 03:06:34 +0000 2020",
+		"id": 1338318292226871300,
+		"id_str": "1338319441784938499",
+		"full_text": "프롬마스 (FROMMARS)\n[돌아갈 수 없었어]\n\n젊은 날 우리들의 설렘과 아쉬움 들을 음악에 담은 밴드 프롬마스 (FROMMARS) ‘돌아갈 수 없었어’. 그 동안 시도하지 않은 새로운 스타일의 곡으로 젊은 날 우리들의 사랑 과 설렘 그리고 아쉬움을 노래한 음악이다.\n\n✚ https://t.co/LwB1LqbQIS https://t.co/Z44zPxGkgj",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			178
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/LwB1LqbQIS",
+					"expanded_url": "https://bit.ly/341Q3d7",
+					"display_url": "bit.ly/341Q3d7",
+					"indices": [
+						155,
+						178
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1337291581976526800,
+					"id_str": "1337291581976526852",
+					"indices": [
+						179,
+						202
+					],
+					"media_url": "http://pbs.twimg.com/media/Eo8Dm8jUwAQLdrn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eo8Dm8jUwAQLdrn.jpg",
+					"url": "https://t.co/Z44zPxGkgj",
+					"display_url": "pic.twitter.com/Z44zPxGkgj",
+					"expanded_url": "https://twitter.com/mirrorballmusic/status/1338318292226871300/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1337291581976526800,
+					"id_str": "1337291581976526852",
+					"indices": [
+						179,
+						202
+					],
+					"media_url": "http://pbs.twimg.com/media/Eo8Dm8jUwAQLdrn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Eo8Dm8jUwAQLdrn.jpg",
+					"url": "https://t.co/Z44zPxGkgj",
+					"display_url": "pic.twitter.com/Z44zPxGkgj",
+					"expanded_url": "https://twitter.com/mirrorballmusic/status/1338318292226871300/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 600,
+							"h": 600,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 125049211,
+			"id_str": "125049211",
+			"name": "미러볼뮤직 Mirrorball Music",
+			"screen_name": "mirrorballmusic",
+			"location": "Republic Korea Mapo-gu",
+			"description": "Korean Indie Music Distributor 미러볼뮤직에서 한국 인디 음악의 모든 것을 만나보세요. https://t.co/7KhvfUdI2i",
+			"url": "https://t.co/Ty0n69rcT2",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Ty0n69rcT2",
+							"expanded_url": "http://www.mirrorballmusic.co.kr",
+							"display_url": "mirrorballmusic.co.kr",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/7KhvfUdI2i",
+							"expanded_url": "http://www.youtube.com/mirrorballmusickorea",
+							"display_url": "youtube.com/mirrorballmusi…",
+							"indices": [
+								62,
+								85
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9512,
+			"friends_count": 2869,
+			"listed_count": 248,
+			"created_at": "Sun Mar 21 14:17:51 +0000 2010",
+			"favourites_count": 954,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 10489,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1285504647734542337/a-q49FRH_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1285504647734542337/a-q49FRH_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/125049211/1603861113",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "프롬마스 (FROMMARS)\n[돌아갈 수 없었어]\n\n젊은 날 우리들의 설렘과 아쉬움 들을 음악에 담은 밴드 프롬마스 (FROMMARS) ‘돌아갈 수 없었어’. 그 동안 시도하지 않은 새로운 스타일의 곡으로 젊은 날 우리들의 사랑 과 설렘 그리고 아쉬움을 노래한 음악이다.\n\n✚ https://t.co/LwB1LqbQIS https://t.co/Z44zPxGkgj"
+	}
+]
