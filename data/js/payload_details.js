2df1e8ba75a435371f311d925ef3700209641985
Fromm <fromm.fancafe@gmail.com>
2022-11-10 00:17:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5178,
-	"created_at": "Sun Oct 30 15:17:05 +0000 2022",
+	"tweets": 5179,
+	"created_at": "Wed Nov 09 15:17:05 +0000 2022",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2022_11.js
@@ -0,0 +1,231 @@
+Grailbird.data.tweets_2022_11 = 
+[
+	{
+		"created_at": "Wed Nov 09 03:17:42 +0000 2022",
+		"id": 1590181809240744000,
+		"id_str": "1590181809240743937",
+		"full_text": "【 🎟️ 】 #MelonTicket #멜론티켓\n프롬 정규 3집 〈Mood，sunday〉 발매기념 쇼케이스 콘서트 (2022.12.04 - 2022.12.04) at 구름아래소극장  \n▶️ https://t.co/Y3DWkSUK2C \n \n#프롬 #Fromm\n#티켓오픈 #공연정보 https://t.co/GgCmmSKvzR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			154
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MelonTicket",
+					"indices": [
+						7,
+						19
+					]
+				},
+				{
+					"text": "멜론티켓",
+					"indices": [
+						20,
+						25
+					]
+				},
+				{
+					"text": "프롬",
+					"indices": [
+						132,
+						135
+					]
+				},
+				{
+					"text": "Fromm",
+					"indices": [
+						136,
+						142
+					]
+				},
+				{
+					"text": "티켓오픈",
+					"indices": [
+						143,
+						148
+					]
+				},
+				{
+					"text": "공연정보",
+					"indices": [
+						149,
+						154
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Y3DWkSUK2C",
+					"expanded_url": "https://ticket.melon.com/performance/index.htm?prodId=207522",
+					"display_url": "ticket.melon.com/performance/in…",
+					"indices": [
+						105,
+						128
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1590181807433064400,
+					"id_str": "1590181807433064450",
+					"indices": [
+						155,
+						178
+					],
+					"media_url": "http://pbs.twimg.com/media/FhF17cBWIAIYMzQ.png",
+					"media_url_https": "https://pbs.twimg.com/media/FhF17cBWIAIYMzQ.png",
+					"url": "https://t.co/GgCmmSKvzR",
+					"display_url": "pic.twitter.com/GgCmmSKvzR",
+					"expanded_url": "https://twitter.com/Fromm_Fancafe/status/1590181809240743937/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 800,
+							"h": 494,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 800,
+							"h": 494,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 420,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1590181807433064400,
+					"id_str": "1590181807433064450",
+					"indices": [
+						155,
+						178
+					],
+					"media_url": "http://pbs.twimg.com/media/FhF17cBWIAIYMzQ.png",
+					"media_url_https": "https://pbs.twimg.com/media/FhF17cBWIAIYMzQ.png",
+					"url": "https://t.co/GgCmmSKvzR",
+					"display_url": "pic.twitter.com/GgCmmSKvzR",
+					"expanded_url": "https://twitter.com/Fromm_Fancafe/status/1590181809240743937/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 800,
+							"h": 494,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 800,
+							"h": 494,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 420,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2202738804,
+			"id_str": "2202738804",
+			"name": "프롬(Fromm) 공식팬카페",
+			"screen_name": "Fromm_Fancafe",
+			"location": "",
+			"description": "🎤 싱어송라이터 #프롬 #Fromm 팬카페",
+			"url": "https://t.co/mUr4gVk6So",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/mUr4gVk6So",
+							"expanded_url": "http://cafe.daum.net/frommeg",
+							"display_url": "cafe.daum.net/frommeg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 268,
+			"friends_count": 25,
+			"listed_count": 5,
+			"created_at": "Tue Nov 19 08:38:40 +0000 2013",
+			"favourites_count": 2630,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4427,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1192311683152109568/MfVUtlJj_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1192311683152109568/MfVUtlJj_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2202738804/1573104234",
+			"profile_link_color": "865C74",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "【 🎟️ 】 #MelonTicket #멜론티켓\n프롬 정규 3집 〈Mood，sunday〉 발매기념 쇼케이스 콘서트 (2022.12.04 - 2022.12.04) at 구름아래소극장  \n▶️ https://t.co/Y3DWkSUK2C \n \n#프롬 #Fromm\n#티켓오픈 #공연정보 https://t.co/GgCmmSKvzR"
+	}
+]
