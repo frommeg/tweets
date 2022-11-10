a931985f2989af7d9520a4910ec7fa1047495a15
Fromm <fromm.fancafe@gmail.com>
2022-11-11 00:17:06 +0900

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5179,
-	"created_at": "Wed Nov 09 15:17:05 +0000 2022",
+	"tweets": 5181,
+	"created_at": "Thu Nov 10 15:17:05 +0000 2022",
 	"lang": "ko"
 }
--- data/js/tweets/2022_11.js
+++ data/js/tweets/2022_11.js
@@ -1,6 +1,490 @@
 Grailbird.data.tweets_2022_11 = 
 [
 	{
+		"created_at": "Wed Nov 09 18:22:00 +0000 2022",
+		"id": 1590409383552647200,
+		"id_str": "1590409383552647168",
+		"full_text": "【 💿 】 #AppleMusic #애플뮤직 \n사랑 아니었나 - Single | 2012년 \n▶️ https://t.co/pCUC8Cnn5H \n \n#프롬 #Fromm \n#뮤직릴리즈 #MusicRelease \n#앨범 #Album https://t.co/Hc7tWAMooz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			125
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "AppleMusic",
+					"indices": [
+						6,
+						17
+					]
+				},
+				{
+					"text": "애플뮤직",
+					"indices": [
+						18,
+						23
+					]
+				},
+				{
+					"text": "프롬",
+					"indices": [
+						81,
+						84
+					]
+				},
+				{
+					"text": "Fromm",
+					"indices": [
+						85,
+						91
+					]
+				},
+				{
+					"text": "뮤직릴리즈",
+					"indices": [
+						93,
+						99
+					]
+				},
+				{
+					"text": "MusicRelease",
+					"indices": [
+						100,
+						113
+					]
+				},
+				{
+					"text": "앨범",
+					"indices": [
+						115,
+						118
+					]
+				},
+				{
+					"text": "Album",
+					"indices": [
+						119,
+						125
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/pCUC8Cnn5H",
+					"expanded_url": "https://music.apple.com/kr/album/%EC%82%AC%EB%9E%91-%EC%95%84%EB%8B%88%EC%97%88%EB%82%98-single/1637919686",
+					"display_url": "music.apple.com/kr/album/%EC%8…",
+					"indices": [
+						54,
+						77
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1590409381555966000,
+					"id_str": "1590409381555965953",
+					"indices": [
+						126,
+						149
+					],
+					"media_url": "http://pbs.twimg.com/media/FhJE5_sXkAEyJu2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FhJE5_sXkAEyJu2.jpg",
+					"url": "https://t.co/Hc7tWAMooz",
+					"display_url": "pic.twitter.com/Hc7tWAMooz",
+					"expanded_url": "https://twitter.com/Fromm_Fancafe/status/1590409383552647168/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1590409381555966000,
+					"id_str": "1590409381555965953",
+					"indices": [
+						126,
+						149
+					],
+					"media_url": "http://pbs.twimg.com/media/FhJE5_sXkAEyJu2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FhJE5_sXkAEyJu2.jpg",
+					"url": "https://t.co/Hc7tWAMooz",
+					"display_url": "pic.twitter.com/Hc7tWAMooz",
+					"expanded_url": "https://twitter.com/Fromm_Fancafe/status/1590409383552647168/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2202738804,
+			"id_str": "2202738804",
+			"name": "프롬(Fromm) 공식팬카페",
+			"screen_name": "Fromm_Fancafe",
+			"location": "",
+			"description": "🎤 싱어송라이터 #프롬 #Fromm 팬카페",
+			"url": "https://t.co/mUr4gVk6So",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/mUr4gVk6So",
+							"expanded_url": "http://cafe.daum.net/frommeg",
+							"display_url": "cafe.daum.net/frommeg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 267,
+			"friends_count": 25,
+			"listed_count": 5,
+			"created_at": "Tue Nov 19 08:38:40 +0000 2013",
+			"favourites_count": 2630,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4430,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1192311683152109568/MfVUtlJj_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1192311683152109568/MfVUtlJj_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2202738804/1573104234",
+			"profile_link_color": "865C74",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "【 💿 】 #AppleMusic #애플뮤직 \n사랑 아니었나 - Single | 2012년 \n▶️ https://t.co/pCUC8Cnn5H \n \n#프롬 #Fromm \n#뮤직릴리즈 #MusicRelease \n#앨범 #Album https://t.co/Hc7tWAMooz"
+	},
+	{
+		"created_at": "Wed Nov 09 18:21:56 +0000 2022",
+		"id": 1590409369660915700,
+		"id_str": "1590409369660915713",
+		"full_text": "【 💿 】 #AppleMusic #애플뮤직 \n너와 나의 - Single | 2013년 \n▶️ https://t.co/zfWtOOoWXM \n \n#프롬 #Fromm \n#뮤직릴리즈 #MusicRelease \n#앨범 #Album https://t.co/BrbGkBe4HB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			123
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "AppleMusic",
+					"indices": [
+						6,
+						17
+					]
+				},
+				{
+					"text": "애플뮤직",
+					"indices": [
+						18,
+						23
+					]
+				},
+				{
+					"text": "프롬",
+					"indices": [
+						79,
+						82
+					]
+				},
+				{
+					"text": "Fromm",
+					"indices": [
+						83,
+						89
+					]
+				},
+				{
+					"text": "뮤직릴리즈",
+					"indices": [
+						91,
+						97
+					]
+				},
+				{
+					"text": "MusicRelease",
+					"indices": [
+						98,
+						111
+					]
+				},
+				{
+					"text": "앨범",
+					"indices": [
+						113,
+						116
+					]
+				},
+				{
+					"text": "Album",
+					"indices": [
+						117,
+						123
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/zfWtOOoWXM",
+					"expanded_url": "https://music.apple.com/kr/album/%EB%84%88%EC%99%80-%EB%82%98%EC%9D%98-single/1637919717",
+					"display_url": "music.apple.com/kr/album/%EB%8…",
+					"indices": [
+						52,
+						75
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1590409367056162800,
+					"id_str": "1590409367056162820",
+					"indices": [
+						124,
+						147
+					],
+					"media_url": "http://pbs.twimg.com/media/FhJE5JrWIAQuviE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FhJE5JrWIAQuviE.jpg",
+					"url": "https://t.co/BrbGkBe4HB",
+					"display_url": "pic.twitter.com/BrbGkBe4HB",
+					"expanded_url": "https://twitter.com/Fromm_Fancafe/status/1590409369660915713/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1590409367056162800,
+					"id_str": "1590409367056162820",
+					"indices": [
+						124,
+						147
+					],
+					"media_url": "http://pbs.twimg.com/media/FhJE5JrWIAQuviE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FhJE5JrWIAQuviE.jpg",
+					"url": "https://t.co/BrbGkBe4HB",
+					"display_url": "pic.twitter.com/BrbGkBe4HB",
+					"expanded_url": "https://twitter.com/Fromm_Fancafe/status/1590409369660915713/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2202738804,
+			"id_str": "2202738804",
+			"name": "프롬(Fromm) 공식팬카페",
+			"screen_name": "Fromm_Fancafe",
+			"location": "",
+			"description": "🎤 싱어송라이터 #프롬 #Fromm 팬카페",
+			"url": "https://t.co/mUr4gVk6So",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/mUr4gVk6So",
+							"expanded_url": "http://cafe.daum.net/frommeg",
+							"display_url": "cafe.daum.net/frommeg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 267,
+			"friends_count": 25,
+			"listed_count": 5,
+			"created_at": "Tue Nov 19 08:38:40 +0000 2013",
+			"favourites_count": 2630,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4430,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1192311683152109568/MfVUtlJj_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1192311683152109568/MfVUtlJj_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2202738804/1573104234",
+			"profile_link_color": "865C74",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "【 💿 】 #AppleMusic #애플뮤직 \n너와 나의 - Single | 2013년 \n▶️ https://t.co/zfWtOOoWXM \n \n#프롬 #Fromm \n#뮤직릴리즈 #MusicRelease \n#앨범 #Album https://t.co/BrbGkBe4HB"
+	},
+	{
 		"created_at": "Wed Nov 09 03:17:42 +0000 2022",
 		"id": 1590181809240744000,
 		"id_str": "1590181809240743937",
