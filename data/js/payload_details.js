6755952b1b54fc3f9018b4f7f7b7294285c63bb0
Fromm <fromm.fancafe@gmail.com>
2023-03-16 00:17:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5236,
-	"created_at": "Thu Mar 02 15:17:15 +0000 2023",
+	"tweets": 5237,
+	"created_at": "Wed Mar 15 15:17:05 +0000 2023",
 	"lang": "ko"
 }
--- data/js/tweets/2023_03.js
+++ data/js/tweets/2023_03.js
@@ -1,6 +1,250 @@
 Grailbird.data.tweets_2023_03 = 
 [
 	{
+		"created_at": "Wed Mar 15 03:07:21 +0000 2023",
+		"id": 1635840075626864600,
+		"id_str": "1635840075626864641",
+		"full_text": "【 📸 】 #Melon #멜론 #멜론포토 \n\n봄밤에 다시만나 함께 부를시간! 서울/ 부산  \n\n▶️ https://t.co/AQ06Em9GEg \n\n원본 : https://t.co/6Im1AUqKe4 \n\n#프롬 #Fromm \n\n#포토 #Photo https://t.co/qY5aGd2yoM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			136
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "Melon",
+					"indices": [
+						6,
+						12
+					]
+				},
+				{
+					"text": "멜론",
+					"indices": [
+						13,
+						16
+					]
+				},
+				{
+					"text": "멜론포토",
+					"indices": [
+						17,
+						22
+					]
+				},
+				{
+					"text": "프롬",
+					"indices": [
+						113,
+						116
+					]
+				},
+				{
+					"text": "Fromm",
+					"indices": [
+						117,
+						123
+					]
+				},
+				{
+					"text": "포토",
+					"indices": [
+						126,
+						129
+					]
+				},
+				{
+					"text": "Photo",
+					"indices": [
+						130,
+						136
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/AQ06Em9GEg",
+					"expanded_url": "https://www.melon.com/artist/photo_detail.htm?artistId=570302&photoId=8020915",
+					"display_url": "melon.com/artist/photo_d…",
+					"indices": [
+						56,
+						79
+					]
+				},
+				{
+					"url": "https://t.co/6Im1AUqKe4",
+					"expanded_url": "https://cdnimg.melon.co.kr/cm2/photo/images/000/802/09/153/80209153_20230315000458_org.jpg",
+					"display_url": "cdnimg.melon.co.kr/cm2/photo/imag…",
+					"indices": [
+						87,
+						110
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1635840073542299600,
+					"id_str": "1635840073542299648",
+					"indices": [
+						137,
+						160
+					],
+					"media_url": "http://pbs.twimg.com/media/FrOr4TzWcAAF_v9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FrOr4TzWcAAF_v9.jpg",
+					"url": "https://t.co/qY5aGd2yoM",
+					"display_url": "pic.twitter.com/qY5aGd2yoM",
+					"expanded_url": "https://twitter.com/Fromm_Fancafe/status/1635840075626864641/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 471,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 710,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 710,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1635840073542299600,
+					"id_str": "1635840073542299648",
+					"indices": [
+						137,
+						160
+					],
+					"media_url": "http://pbs.twimg.com/media/FrOr4TzWcAAF_v9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FrOr4TzWcAAF_v9.jpg",
+					"url": "https://t.co/qY5aGd2yoM",
+					"display_url": "pic.twitter.com/qY5aGd2yoM",
+					"expanded_url": "https://twitter.com/Fromm_Fancafe/status/1635840075626864641/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 471,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 710,
+							"h": 1024,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 710,
+							"h": 1024,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2202738804,
+			"id_str": "2202738804",
+			"name": "프롬(Fromm) 공식팬카페",
+			"screen_name": "Fromm_Fancafe",
+			"location": "",
+			"description": "🎤 싱어송라이터 #프롬 #Fromm 팬카페",
+			"url": "https://t.co/mUr4gVk6So",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/mUr4gVk6So",
+							"expanded_url": "http://cafe.daum.net/frommeg",
+							"display_url": "cafe.daum.net/frommeg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 260,
+			"friends_count": 25,
+			"listed_count": 4,
+			"created_at": "Tue Nov 19 08:38:40 +0000 2013",
+			"favourites_count": 2621,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4493,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1192311683152109568/MfVUtlJj_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1192311683152109568/MfVUtlJj_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2202738804/1573104234",
+			"profile_link_color": "865C74",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "【 📸 】 #Melon #멜론 #멜론포토 \n\n봄밤에 다시만나 함께 부를시간! 서울/ 부산  \n\n▶️ https://t.co/AQ06Em9GEg \n\n원본 : https://t.co/6Im1AUqKe4 \n\n#프롬 #Fromm \n\n#포토 #Photo https://t.co/qY5aGd2yoM"
+	},
+	{
 		"created_at": "Thu Mar 02 14:09:38 +0000 2023",
 		"id": 1631295703946260500,
 		"id_str": "1631295703946260480",
