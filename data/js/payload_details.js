95a5b66eceaaea6dad8743294850bd46f78fbf75
Fromm <fromm.fancafe@gmail.com>
2023-04-22 00:17:06 +0900

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 5237,
-	"created_at": "Wed Mar 15 15:17:05 +0000 2023",
+	"tweets": 5238,
+	"created_at": "Fri Apr 21 15:17:05 +0000 2023",
 	"lang": "ko"
 }
---
+++ data/js/tweets/2023_04.js
@@ -0,0 +1,231 @@
+Grailbird.data.tweets_2023_04 = 
+[
+	{
+		"created_at": "Fri Apr 21 03:18:34 +0000 2023",
+		"id": 1649251248971956200,
+		"id_str": "1649251248971956226",
+		"full_text": "【 🎟️ 】 #MelonTicket #멜론티켓\n\n2023 라라라온－드림홀 (2023.05.10 - 2023.05.11) at 드림홀  \n\n▶️ https://t.co/qVRASl8JQ0 \n\n#프롬 #Fromm\n\n#티켓오픈 #공연정보 https://t.co/zOArKm3ZmA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			129
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MelonTicket",
+					"indices": [
+						7,
+						19
+					]
+				},
+				{
+					"text": "멜론티켓",
+					"indices": [
+						20,
+						25
+					]
+				},
+				{
+					"text": "프롬",
+					"indices": [
+						106,
+						109
+					]
+				},
+				{
+					"text": "Fromm",
+					"indices": [
+						110,
+						116
+					]
+				},
+				{
+					"text": "티켓오픈",
+					"indices": [
+						118,
+						123
+					]
+				},
+				{
+					"text": "공연정보",
+					"indices": [
+						124,
+						129
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/qVRASl8JQ0",
+					"expanded_url": "https://ticket.melon.com/performance/index.htm?prodId=208111",
+					"display_url": "ticket.melon.com/performance/in…",
+					"indices": [
+						80,
+						103
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1649251247046754300,
+					"id_str": "1649251247046754305",
+					"indices": [
+						130,
+						153
+					],
+					"media_url": "http://pbs.twimg.com/media/FuNRRWWXgAEBq6T.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FuNRRWWXgAEBq6T.jpg",
+					"url": "https://t.co/zOArKm3ZmA",
+					"display_url": "pic.twitter.com/zOArKm3ZmA",
+					"expanded_url": "https://twitter.com/Fromm_Fancafe/status/1649251248971956226/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 420,
+							"h": 594,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 420,
+							"h": 594,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 420,
+							"h": 594,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1649251247046754300,
+					"id_str": "1649251247046754305",
+					"indices": [
+						130,
+						153
+					],
+					"media_url": "http://pbs.twimg.com/media/FuNRRWWXgAEBq6T.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/FuNRRWWXgAEBq6T.jpg",
+					"url": "https://t.co/zOArKm3ZmA",
+					"display_url": "pic.twitter.com/zOArKm3ZmA",
+					"expanded_url": "https://twitter.com/Fromm_Fancafe/status/1649251248971956226/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 420,
+							"h": 594,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 420,
+							"h": 594,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 420,
+							"h": 594,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2202738804,
+			"id_str": "2202738804",
+			"name": "프롬(Fromm) 공식팬카페",
+			"screen_name": "Fromm_Fancafe",
+			"location": "",
+			"description": "🎤 싱어송라이터 #프롬 #Fromm 팬카페",
+			"url": "https://t.co/mUr4gVk6So",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/mUr4gVk6So",
+							"expanded_url": "http://cafe.daum.net/frommeg",
+							"display_url": "cafe.daum.net/frommeg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": true,
+			"followers_count": 259,
+			"friends_count": 24,
+			"listed_count": 4,
+			"created_at": "Tue Nov 19 08:38:40 +0000 2013",
+			"favourites_count": 2613,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4494,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F5F5",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1192311683152109568/MfVUtlJj_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1192311683152109568/MfVUtlJj_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2202738804/1573104234",
+			"profile_link_color": "865C74",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none",
+			"withheld_in_countries": []
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "ko",
+		"text": "【 🎟️ 】 #MelonTicket #멜론티켓\n\n2023 라라라온－드림홀 (2023.05.10 - 2023.05.11) at 드림홀  \n\n▶️ https://t.co/qVRASl8JQ0 \n\n#프롬 #Fromm\n\n#티켓오픈 #공연정보 https://t.co/zOArKm3ZmA"
+	}
+]
